4c585a1fca98888176c30c9a0013feb4cf94d438
RxSajib <sajibroy206@gmail.com>
2026-06-13 13:14:00 +0600

    feat(ui): component design SectionHeader

--- app/src/main/java/com/zenbyte/studio/digitalwallet/ui/screen/HomScreen.kt
+++ app/src/main/java/com/zenbyte/studio/digitalwallet/ui/screen/HomScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -12,11 +13,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.LocalPlatformContext
+import com.zenbyte.studio.digitalwallet.R
 import com.zenbyte.studio.digitalwallet.ui.component.MerchantItem
+import com.zenbyte.studio.digitalwallet.ui.component.SectionHeader
 import com.zenbyte.studio.presentation.viewmodel.home.HomeViewModel
 import com.zenbyte.studio.presentation.viewmodel.merchant.MerchantViewModel
 
@@ -30,6 +34,11 @@ fun HomeScreen() {
 
 
         item {
+
+            SectionHeader(title = stringResource(R.string.nearby_merchant), modifier = Modifier.padding(horizontal = 16.dp)){
+
+            }
+
             LazyRow(contentPadding = PaddingValues(horizontal = 16.dp), modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(10.dp)) {
                 items(merchantList.value){merchant ->
                     MerchantItem(merchant = merchant, context = contentCoil)
